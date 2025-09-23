8ec91cac4be42c7960918cb8191a629c362ee068
Aurelie Raout <aurelieraout@gmail.com>
2025-09-23 17:36:45 +0200

    boucle for sur les articles

--- pieces.js
+++ pieces.js
@@ -1,31 +1,27 @@
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
-const article = pieces[0];
 
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
-
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
-
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
-
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
-
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
-
-const disponibiliteElement = document.createElement("p");
-disponibiliteElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
-
-// Ajout des éléments au DOM
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(disponibiliteElement);
+for (let i = 0; i < pieces.length; i++) {
+  const sectionFiches = document.querySelector(".fiches");
+  const pieceElement = document.createElement("article");
+  const imageElement = document.createElement("img");
+  const nomElement = document.createElement("h2");
+  const prixElement = document.createElement("p");
+  const categorieElement = document.createElement("p");
+  const descriptionElement = document.createElement("p");
+  const disponibiliteElement = document.createElement("p");
+  imageElement.src = pieces[i].image;
+  sectionFiches.appendChild(pieceElement);
+  pieceElement.appendChild(imageElement);
+  nomElement.innerText = pieces[i].nom;
+  pieceElement.appendChild(nomElement);
+  prixElement.innerText = `Prix: ${pieces[i].prix} € (${pieces[i].prix < 35 ? "€" : "€€€"})`;
+  pieceElement.appendChild(prixElement);
+  categorieElement.innerText = pieces[i].categorie ?? "(aucune catégorie)";
+  pieceElement.appendChild(categorieElement);
+  descriptionElement.innerText = pieces[i].description ?? "Pas de description pour le moment.";
+  pieceElement.appendChild(descriptionElement);
+  disponibiliteElement.innerText = pieces[i].disponibilite ? "En stock" : "Rupture de stock";
+  pieceElement.appendChild(disponibiliteElement);
+}
